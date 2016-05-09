864b85b8195faff6b19bd16531fa95751eb1af42
laichunpongben <laichunpongben@gmail.com>
2016-05-09 11:21:44 +0800

    Create convert-xml-to-csv.py

---
+++ convert-xml-to-csv.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from lxml import etree
+import csv
+
+repository = r'E:\stackoverflow' #Your repository
+file_names = ['Badges', 'Comments', 'Posts', 'PostLinks', 'Tags', 'Users', 'Votes']
+header_dict = {
+    'Badges': ('Id', 'UserId', 'Name', 'Date', 'Class', 'TagBased'),
+    'Comments': ('Id', 'PostId', 'Score', 'TextLength', 'CreationDate', 'UserId'),
+    'Posts': ('Id', 'PostTypeId', 'ParentId', 'AcceptedAnswerId', 'CreationDate', 'Score', 'ViewCount', 'BodyLength', 'OwnerUserId', 
+                'LastEditorUserId', 'LastEditorDisplayName', 'LastEditorDisplayName', 'LastEditDate', 'LastActivityDate', 'CommunityOwnedDate', 'ClosedDate', 
+                'TitleLength', 'Tags', 'AnswerCount', 'CommentCount', 'FavoriteCount'),
+    'PostLinks': ('Id', 'CreationDate', 'PostId', 'RelatedPostId', 'LinkTypeId'),
+    'Tags': ('Id', 'TagName', 'Count', 'ExcerptPostId', 'WikiPostId'),
+    'Users': ('Id', 'Reputation', 'CreationDate', 'DisplayName', 'LastAccessDate', 'WebSiteUrl', 'Location', 'Age', 'Views', 'UpVotes', 'DownVotes'),
+    'Votes': ('Id', 'PostId', 'VoteTypeId', 'CreationDate')
+}
+convert_dict = { 
+    'Badges': False, 
+    'Comments': False, 
+    'Posts': False, 
+    'PostLinks': False, 
+    'Tags': False, 
+    'Users': False,
+    'Votes': True
+} #Set to True if convert xml to csv 
+
+class Stackoverflow(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.xml_file = os.path.join(repository, str(self)+'.xml')
+        self.csv_file = os.path.join(repository, str(self)+'_.csv')
+        self.header = header_dict[str(self)]
+        
+    def __str__(self):
+        return self.file_name
+        
+    def make_csv(self):
+        with open(self.csv_file, 'wb') as file_:
+            writer = csv.writer(file_, delimiter=',')
+            writer.writerow(self.header)
+            
+            parser = etree.iterparse(self.xml_file)
+            for event, row in parser:
+                if row.tag=='row':
+                    row_content = (row.attrib.get(attribute, '') for attribute in self.header)
+                    writer.writerow([unicode(x).encode('utf-8') for x in row_content])
+                row.clear()
+                while row.getprevious() is not None:
+                    del row.getparent()[0]
+            del parser
+    
+if __name__=='__main__':
+    stackoverflows = [Stackoverflow(file_name) for file_name in file_names]
+    for stackoverflow in stackoverflows:
+        if convert_dict[str(stackoverflow)]:
+            stackoverflow.make_csv()
